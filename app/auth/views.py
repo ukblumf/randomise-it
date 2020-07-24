0c3f208da1e9e02d0f9062329605ce2ebb6ac07d
blumf <neil.blumfield@gmail.com>
2020-07-24 19:48:07 +0100

    Registration fix 4

--- app/auth/views.py
+++ app/auth/views.py
@@ -39,13 +39,10 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user is None:
-            flash('Invalid username')
-        elif user.verify_password(form.password.data):
+        if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-        else:
-            flash('Invalid password.')
+        flash('Sorry, something was not right.')
     return render_template('auth/login.html', form=form)
 
 
@@ -64,12 +61,12 @@ def register():
         user = User(email=form.email.data,
                     username=form.username.data,
                     password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
         token = user.generate_confirmation_token()
         send_email(user.email, 'Confirm Your Account',
                    'auth/email/confirm', user=user, token=token)
         flash('A confirmation email has been sent to you by email.')
-        db.session.add(user)
-        db.session.commit()
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
 
