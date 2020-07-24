2d70a4493e5f98eca16f541fb8fd561b324ecd38
blumf <neil.blumfield@gmail.com>
2020-07-24 18:59:14 +0100

    Reinstated comments/follows

--- app/models.py
+++ app/models.py
@@ -64,13 +64,13 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-# class Follow(db.Model):
-#     __tablename__ = 'follows'
-#     follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),
-#                             primary_key=True)
-#     followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),
-#                             primary_key=True)
-#     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                            primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                            primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 class User(UserMixin, db.Model):
@@ -109,17 +109,17 @@ class User(UserMixin, db.Model):
                                                 backref=db.backref('original_author', lazy='joined'),
                                                 lazy='dynamic',
                                                 cascade='all, delete-orphan')
-    # followed = db.relationship('Follow',
-    #                            foreign_keys=[Follow.follower_id],
-    #                            backref=db.backref('follower', lazy='joined'),
-    #                            lazy='dynamic',
-    #                            cascade='all, delete-orphan')
-    # followers = db.relationship('Follow',
-    #                             foreign_keys=[Follow.followed_id],
-    #                             backref=db.backref('followed', lazy='joined'),
-    #                             lazy='dynamic',
-    #                             cascade='all, delete-orphan')
-    # comments = db.relationship('Comment', backref='author', lazy='dynamic')
+    followed = db.relationship('Follow',
+                               foreign_keys=[Follow.follower_id],
+                               backref=db.backref('follower', lazy='joined'),
+                               lazy='dynamic',
+                               cascade='all, delete-orphan')
+    followers = db.relationship('Follow',
+                                foreign_keys=[Follow.followed_id],
+                                backref=db.backref('followed', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
+    comments = db.relationship('Comment', backref='author', lazy='dynamic')
 
     @staticmethod
     def generate_fake(count=100):
@@ -143,13 +143,13 @@ class User(UserMixin, db.Model):
             except IntegrityError:
                 db.session.rollback()
 
-    # @staticmethod
-    # def add_self_follows():
-    #     for user in User.query.all():
-    #         if not user.is_following(user):
-    #             user.follow(user)
-    #             db.session.add(user)
-    #             db.session.commit()
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -161,6 +161,7 @@ class User(UserMixin, db.Model):
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(
                 self.email.encode('utf-8')).hexdigest()
+        self.followed.append(Follow(followed=self))
 
     @property
     def password(self):
@@ -249,28 +250,28 @@ class User(UserMixin, db.Model):
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating)
 
-    # def follow(self, user):
-    #     if not self.is_following(user):
-    #         f = Follow(follower=self, followed=user)
-    #         db.session.add(f)
-    #
-    # def unfollow(self, user):
-    #     f = self.followed.filter_by(followed_id=user.id).first()
-    #     if f:
-    #         db.session.delete(f)
-    #
-    # def is_following(self, user):
-    #     return self.followed.filter_by(
-    #         followed_id=user.id).first() is not None
-    #
-    # def is_followed_by(self, user):
-    #     return self.followers.filter_by(
-    #         follower_id=user.id).first() is not None
+    def follow(self, user):
+        if not self.is_following(user):
+            f = Follow(follower=self, followed=user)
+            db.session.add(f)
 
-    # @property
-    # def followed_posts(self):
-    #     return Post.query.join(Follow, Follow.followed_id == Post.author_id) \
-    #         .filter(Follow.follower_id == self.id)
+    def unfollow(self, user):
+        f = self.followed.filter_by(followed_id=user.id).first()
+        if f:
+            db.session.delete(f)
+
+    def is_following(self, user):
+        return self.followed.filter_by(
+            followed_id=user.id).first() is not None
+
+    def is_followed_by(self, user):
+        return self.followers.filter_by(
+            follower_id=user.id).first() is not None
+
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id) \
+            .filter(Follow.follower_id == self.id)
 
     def to_json(self):
         json_user = {
@@ -279,6 +280,8 @@ class User(UserMixin, db.Model):
             'member_since': self.member_since,
             'last_seen': self.last_seen,
             'posts': url_for('api.get_user_posts', id=self.id, _external=True),
+            'followed_posts': url_for('api.get_user_followed_posts',
+                                      id=self.id, _external=True),
             'post_count': self.posts.count()
         }
         return json_user
@@ -366,43 +369,43 @@ class Post(db.Model):
 db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 
-# class Comment(db.Model):
-#     __tablename__ = 'comments'
-#     id = db.Column(db.Integer, primary_key=True)
-#     body = db.Column(db.Text)
-#     body_html = db.Column(db.Text)
-#     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-#     disabled = db.Column(db.Boolean)
-#     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-#
-#     @staticmethod
-#     def on_changed_body(target, value, oldvalue, initiator):
-#         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'code', 'em', 'i',
-#                         'strong']
-#         target.body_html = bleach.linkify(bleach.clean(
-#             markdown(value, output_format='html'),
-#             tags=allowed_tags, strip=True))
-#
-#     def to_json(self):
-#         json_comment = {
-#             'url': url_for('api.get_comment', id=self.id, _external=True),
-#             'body': self.body,
-#             'body_html': self.body_html,
-#             'timestamp': self.timestamp,
-#             'author': url_for('api.get_user', id=self.author_id,
-#                               _external=True),
-#         }
-#         return json_comment
-#
-#     @staticmethod
-#     def from_json(json_comment):
-#         body = json_comment.get('body')
-#         if body is None or body == '':
-#             raise ValidationError('comment does not have a body')
-#         return Comment(body=body)
-#
-#
-# db.event.listen(Comment.body, 'set', Comment.on_changed_body)
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    disabled = db.Column(db.Boolean)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'code', 'em', 'i',
+                        'strong']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+
+    def to_json(self):
+        json_comment = {
+            'url': url_for('api.get_comment', id=self.id, _external=True),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author': url_for('api.get_user', id=self.author_id,
+                              _external=True),
+        }
+        return json_comment
+
+    @staticmethod
+    def from_json(json_comment):
+        body = json_comment.get('body')
+        if body is None or body == '':
+            raise ValidationError('comment does not have a body')
+        return Comment(body=body)
+
+
+db.event.listen(Comment.body, 'set', Comment.on_changed_body)
 
 
 class RandomTable(db.Model):
