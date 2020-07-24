877f11ebd98cb35f21c525f1eccd970a57fc0413
blumf <neil.blumfield@gmail.com>
2020-07-24 19:23:49 +0100

    Registration fix 3

--- app/auth/views.py
+++ app/auth/views.py
@@ -39,10 +39,13 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user is not None and user.verify_password(form.password.data):
+        if user is None:
+            flash('Invalid username')
+        elif user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid username or password.')
+        else:
+            flash('Invalid password.')
     return render_template('auth/login.html', form=form)
 
 
